109d23406e9564c4aacc87b86b34d4168a9b8d9a
AnthonyCastilloCD <pcastilloa@campusdigitalfp.com>
2025-01-18 01:14:21 +0100

    Movement Of Enemy Between Locations

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -1,9 +1,46 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class EnemyBehaviour : MonoBehaviour
 {
+    //References Patrol
+    public Transform PatrolRoute;
+    public List <Transform> Locations;
+    private int _locationIndex = 0;
+    private NavMeshAgent _agent;
+
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        InitializePatrolRoute();
+        MoveToNextPatrolLocation();
+    }
+
+    void Update()
+    {
+        if(_agent.remainingDistance < 0.2f && !_agent.pathPending)
+        {
+            MoveToNextPatrolLocation();
+        }
+    }
+
+    void InitializePatrolRoute()
+    {
+        foreach (Transform child in PatrolRoute)
+        {
+            Locations.Add (child);
+        }
+    }
+    void MoveToNextPatrolLocation()
+    {
+        if (Locations.Count == 0)
+        return;
+        _agent.destination = Locations [_locationIndex].position;
+        _locationIndex = (_locationIndex + 1) % Locations.Count;
+    }
  void  OnTriggerEnter(Collider other)
  {
     if(other.name == "Player")
